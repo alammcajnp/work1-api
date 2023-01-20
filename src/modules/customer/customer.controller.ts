3bff22dec273c800002df965ba46d0675bd525fd
alammcajnp <alamjnp12@gmail.com>
2023-01-20 23:57:59 +0530

    changes

--- src/modules/customer/customer.controller.ts
+++ src/modules/customer/customer.controller.ts
@@ -23,7 +23,7 @@ export class CustomerController {
   async get(@Res() res) {
     return res
       .status(HttpStatus.OK)
-      .json({ message: 'getting data from customer 1' });
+      .json({ message: 'getting data from customer 2' });
   }
 
   // add a customer
